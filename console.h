387f8e0a88adae9366b532a321f76349be790a6a
kimjongdill <george.a.dill@gmail.com>
2017-11-18 17:27:02 -0600

    Update console.h

--- console.h
+++ console.h
@@ -1,21 +1,13 @@
 #include <list>
 #include <algorithm>
+#include <ArduinoJson.h>
 
 typedef struct console CONSOLE;
 typedef struct node NODE;
 
-extern void console_delete_id(CONSOLE* c, uint32_t id);
-extern void console_push(CONSOLE* c, uint32_t id, int data, int type);
-extern void console_free(CONSOLE* c);
+
 extern CONSOLE* console_init();
-extern int console_id_exist(CONSOLE* c, uint32_t id);
-extern void console_update_node(CONSOLE* c, uint32_t id, int data, int type);
-extern void console_update_node_list(CONSOLE* c, std::list<uint32_t> l);
-
-extern void console_get_next_alarm(CONSOLE* c);
-
-extern void console_get_next_console(CONSOLE* c, String *name, int *data);
-
-extern void console_get_next_controller(CONSOLE* c);
-
-extern void console_get_next_doorbell(CONSOLE* c);
+extern int console_update(CONSOLE* c, String msg);
+extern void console_print(CONSOLE* c);
+extern void console_get_stats(CONSOLE* c, int* minimum, int* avg);
+extern void console_get_info(CONSOLE* c, int* index, char name[100], int* temp);
