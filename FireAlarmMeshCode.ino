f3cf27470600c583adf9acb64f43cc6fde1448d5
ozorob2 <omar.zorob@yahoo.com>
2017-11-28 10:34:06 -0600

    Update FireAlarmMeshCode.ino

--- FireAlarmMeshCode.ino
+++ FireAlarmMeshCode.ino
@@ -12,9 +12,9 @@
 #define   MESH_PREFIX     "whateverYouLike"
 #define   MESH_PASSWORD   "somethingSneaky"
 #define   MESH_PORT       5555
-#define   BTN             16
+#define   BTN             5
 
-bool currentlyAlarming = false;
+bool currentlyAlarming;
 
 void sendMessage() ; // Prototype so PlatformIO doesn't complain
 
@@ -40,6 +40,8 @@ void nodeTimeAdjustedCallback(int32_t offset) {
 }
 
 void setup() {
+  pinMode(BTN, INPUT);
+  currentlyAlarming = false;
   Serial.begin(115200);
   attachInterrupt(BTN, resetCurAlarm, RISING);
 
@@ -67,8 +69,9 @@ void resetCurAlarm(){
 int getAlarmBool(){
   int alarmThreshold = 335;
   int analogSensor = analogRead(A0);
-  //Serial.print("Pin A0: ");
-  //Serial.println(analogSensor);
+  Serial.print("Pin A0: ");
+  Serial.println(analogSensor);
+  Serial.println(currentlyAlarming);
 
   if(analogSensor > alarmThreshold)
     currentlyAlarming = true;
